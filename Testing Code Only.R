1d209fd1192f38a055ebee1f47a87ba169e9a804
sarjit94 <sarjit94@icloud.com>
2023-11-11 17:28:29 +0000

    Now able to lookup checking list and see which codes are not in the list. Next steps will be to be able to dump a list in and run the code.

--- Testing Code Only.R
+++ Testing Code Only.R
@@ -1,4 +1,4 @@
-##### LOAD LIBRARIES / SETUP R ------------------------------------------------
+##### LOAD LIBRARIES / SETUP R ----------------------------------------------
 # Clear R 
 rm(list=ls())
 graphics.off()
@@ -6,24 +6,66 @@ graphics.off()
 library(tidyverse)
 
 
-##### SETUP OPTIONS -----------------------------------------------------------
-# Location of the codelist to check
-cl_checking <- readxl::read_excel("HTN/HTN Rx codes.xlsx")
+# CREATE FUNCTIONS --------------------------------------------------------
+# Create a function to remove subcodes (eg B22 if B2 exists in list)
+remove_subcodes <- function(df, code_column) {
+  # Create a vector of unique codes
+  codes <- unique(df[[code_column]])
+  # Define a function to determine if a code is a subcode of any other
+  is_subcode <- function(code, codes) {
+    # Check if there are any codes that are a substring of the current code
+    any(sapply(codes, function(x) grepl(paste0("^", x), code) && x != code))
+  }
+  # Filter out subcodes
+  filtered_codes <- codes[!sapply(codes, is_subcode, codes = codes)]
+  # Filter the original dataframe to keep only rows with the filtered codes
+  df_filtered <- df %>% 
+    filter(get(code_column) %in% filtered_codes)
+  return(df_filtered)
+}
 
-# Import BioBank conversion table  
-biobank <- readxl::read_excel("BioBank/biobank.xlsx", sheet = "read_v2_drugs_bnf")
-biobank$bnfcode_new <- str_remove_all(biobank$bnf_code, "\\.")
+
+##### IMPORT KNOWN DATABASES  -----------------------------------------------
+# Location of the code list to check
+# As importing, create new variables with periods removed (easier to work with)
+cl_checking <- readxl::read_excel("HTN/HTN Rx codes.xlsx") %>%
+  pull(Code) %>%
+  str_remove_all(., "\\.")
+
+# Import BioBank conversion table
+# As importing, create new variables with periods removed (easier to work with)
+biobank <- 
+  readxl::read_excel("BioBank/biobank.xlsx", sheet = "read_v2_drugs_bnf") %>%
+  mutate(
+    readcode_new = str_remove_all(read_code, "\\."),
+    bnfcode_new = str_remove_all(bnf_code, "\\."))
+
+# Import BioBank's Read code lookup table
+read_lookup <- 
+  readxl::read_excel("BioBank/biobank.xlsx", sheet = "read_v2_lkp") %>%
+  mutate(
+    read_code = str_remove_all(read_code, "\\."))
 
 
-# First validation set
-res24_beta_blockers <- read_csv("HTN/Validation List/res24-beta_blockers.csv")
-
-# Remove quotation marks
-res24_beta_blockers$bnfcode <- str_remove_all(res24_beta_blockers$bnfcode, '"')
-
-list_res24_beta_blockers <- str_split(res24_beta_blockers$bnfcode, pattern = "/", simplify = TRUE) %>% 
-  as.list() %>%
+# FIRST VALIDATION LIST ---------------------------------------------------
+# Import the testing code list and pull out the BNF code column
+# Then split that column based on the presence of slashes
+# Then remove empty values (.[. !=""])
+res24_beta_blockers <-
+  read_csv("HTN/Validation List/res24-beta_blockers.csv") %>%
+  pull(bnfcode) %>%
+  str_split(., pattern = "/", simplify = TRUE) %>%
   .[. != ""]
+
+
+# Cross check the list imported againt the biobank BNF code list
+# Then remove Read subcodes and pull out these values
+# Then check if these codes don't exist in the cl_checking vector
+temp <- biobank %>%
+  filter(bnfcode_new %in% res24_beta_blockers) %>%
+  remove_subcodes("readcode_new") %>%
+  pull(readcode_new) %>%
+  .[!(. %in% cl_checking)]
 
 
 
